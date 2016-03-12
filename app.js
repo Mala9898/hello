aa119fea8d5bf1ed59b7beeac2a2766e7fc545dc
Stan Malec <smgoldskate@gmail.com>
2016-03-12 13:37:58 +0100

    g

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.set('port', 8080);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.set('port', 8080);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
