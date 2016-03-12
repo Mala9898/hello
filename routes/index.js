057a6d9547647919bef92236c0dd1920c7589e3d
Stan Malec <smgoldskate@gmail.com>
2016-03-12 15:29:22 +0100

    g

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var router = express.Router()
-var request = require('request')
+var request = require('request');
 var jsdom = require('jsdom')
 var perfy = require('perfy')
+var fs = require('fs')
+var rp = require('request-promise')
+var cheerio = require('cheerio')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -49,32 +52,51 @@ router.get('/', function(req, res, next) {
 
 //http://www.adidas.com/se/apps/yeezy/
 router.get('/yay', function(req,res){
-	perfy.start('loop-stuff');
-	request({uri: 'http://www.adidas.se/nmd?grid=true'}, function(err, response, body){
-            var self = this;
-        	self.items = new Array();//I feel like I want to save my results in an array
+
+	var options = {
+	    uri: 'http://www.adidas.se/nmd?grid=true',
+	    headers: {
+        'User-Agent': 'Request-Promise'
+    	},
+	    transform: function (body) {
+	        return cheerio.load(body);
+	    }
+	};
+ 
+	rp(options)
+	    .then(function ($) {
+	    	res.send($('span[class=title]').text())
+	        console.log("this: "+$('span[class=title]').text()) 
+	    })
+	    .catch(function (err) {
+	        console.log("fail"+err)
+	    });
+	// perfy.start('loop-stuff');
+	// request({uri: 'http://www.adidas.se/nmd?grid=true'}, function(err, response, body){
+ //            var self = this;
+ //        	self.items = new Array();//I feel like I want to save my results in an array
          
-        	//Just a basic error check
-            if(err && response.statusCode !== 200){console.log('Request error.');}
+ //        	//Just a basic error check
+ //            if(err && response.statusCode !== 200){console.log('Request error.');}
 
-            fs.writeFile('normal.txt', body, function (err) {
-			  if (err) return console.log(err);
-			  console.log('Hello World > helloworld.txt');
-			});
-                //Send the body param as the HTML code we will parse in jsdom
-        	//also tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
-    		// console.log("succes: " + body)
+ //            fs.writeFile('normal.txt', body, function (err) {
+	// 		  if (err) return console.log(err);
+	// 		  console.log('Hello World > helloworld.txt');
+	// 		});
+ //                //Send the body param as the HTML code we will parse in jsdom
+ //        	//also tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
+ //    		// console.log("succes: " + body)
 
-    		jsdom.env(
-			  body,
-			  ["http://code.jquery.com/jquery.js"],
-			  function (err, window) {
-			    //console.log("contents of a.the-link:", window.$("a").text());
-			    res.send(window.$("p").text())
-			    var result = perfy.end('loop-stuff');
-				console.log(result.summary);
-			})
-        });
+ //    		jsdom.env(
+	// 		  body,
+	// 		  ["http://code.jquery.com/jquery.js"],
+	// 		  function (err, window) {
+	// 		    //console.log("contents of a.the-link:", window.$("a").text());
+	// 		    res.send(window.$("h1").text())
+	// 		    var result = perfy.end('loop-stuff');
+	// 			console.log(result.summary);
+	// 		})
+ //        });
 	
 })
 
